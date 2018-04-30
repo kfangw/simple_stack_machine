b7e070bb8c6157f2aa718cda32e45a33a5a896e8
kfangw <kfangw@gmail.com>
2018-04-30 13:30:07 +0900

    python 3

--- stack_machine/test/test_codegen.py
+++ stack_machine/test/test_codegen.py
@@ -4,8 +4,6 @@ import os
 
 import pytest
 
-from stack_machine.ast import TopAST, NumberExprAST, StmtNopAST, StmtDropAST, StmtDupAST, StmtPushAST, StmtNotAST, \
-    StmtXorAST, StmtOrAST, StmtAndAST, StmtSubAST, StmtAddAST
 from stack_machine.codegen import CodeGen
 from stack_machine.parser import Parser
 
@@ -14,34 +12,17 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.usefixtures("clean_up_files")
 @pytest.mark.parametrize("input_str,expected", [
-    ("push 1 push 2 push 3", "[sp:2][value:3]"),
-    ("push 1 push 2 add",    "[sp:0][value:3]"),
-    ("push 1 push 2 sub",    "[sp:0][value:-1]"),
-    ("push 1 push 2 and",    "[sp:0][value:0]"),
-    ("push 1 push 3 and",    "[sp:0][value:1]"),
-    ("push 1 push 2 or",     "[sp:0][value:3]"),
-    ("push 1 push 3 or",     "[sp:0][value:3]"),
-    ("push 1 push 4 or",     "[sp:0][value:5]"),
-    ("push 1 push 2 xor",    "[sp:0][value:3]"),
-    ("push 1 push 3 xor",    "[sp:0][value:2]"),
-    ("push 1 push 3 not",    "[sp:1][value:-4]"),
-    ("push 1 push 2 dup",    "[sp:2][value:2]"),
-    ("push 1 push 2 drop",   "[sp:0][value:1]"),
-    ("push 1",                                                                        "[sp:0][value:1]"),
-    ("nop push 1",                                                                    "[sp:0][value:1]"),
-    ("nop push 1 push 2",                                                             "[sp:1][value:2]"),
-    ("nop push 1 push 2 add",                                                         "[sp:0][value:3]"),
-    ("nop push 1 push 2 add push 3",                                                  "[sp:1][value:3]"),
-    ("nop push 1 push 2 add push 3 sub",                                              "[sp:0][value:0]"),
-    ("nop push 1 push 2 add push 3 sub push 4",                                       "[sp:1][value:4]"),
-    ("nop push 1 push 2 add push 3 sub push 4 and",                                   "[sp:0][value:0]"),
-    ("nop push 1 push 2 add push 3 sub push 4 and push 5",                            "[sp:1][value:5]"),
-    ("nop push 1 push 2 add push 3 sub push 4 and push 5 or",                         "[sp:0][value:5]"),
-    ("nop push 1 push 2 add push 3 sub push 4 and push 5 or push 6",                  "[sp:1][value:6]"),
-    ("nop push 1 push 2 add push 3 sub push 4 and push 5 or push 6 xor",              "[sp:0][value:3]"),
-    ("nop push 1 push 2 add push 3 sub push 4 and push 5 or push 6 xor not",          "[sp:0][value:-4]"),
-    ("nop push 1 push 2 add push 3 sub push 4 and push 5 or push 6 xor not dup",      "[sp:1][value:-4]"),
-    ("nop push 1 push 2 add push 3 sub push 4 and push 5 or push 6 xor not dup drop", "[sp:0][value:-4]"),
+    ("push 1 push 2 push 3 print",                  "[sp:2][value:3]"),
+    ("push 1 push 2 add print",                     "[sp:0][value:3]"),
+    ("push 1 push 2 sub print",                     "[sp:0][value:-1]"),
+    ("push 1 push 2 dup print",                     "[sp:2][value:2]"),
+    ("push 1 push 2 drop print",                    "[sp:0][value:1]"),
+    ("push 1 print",                                "[sp:0][value:1]"),
+    ("push 1 push 2 print",                         "[sp:1][value:2]"),
+    ("push 1 push 2 add print",                     "[sp:0][value:3]"),
+    ("push 1 push 2 add push 3 print",              "[sp:1][value:3]"),
+    ("push 1 push 2 add push 3 sub print",          "[sp:0][value:0]"),
+    ("push 1 push 2 add push 3 sub push 4 print",   "[sp:1][value:4]"),
 ])
 def test_parser(input_str, expected):
     parser = Parser()
--- stack_machine/test/test_lexer.py
+++ stack_machine/test/test_lexer.py
@@ -3,24 +3,17 @@ from stack_machine.lexer import Lexer
 
 
 def test_lexer():
-    lexer = Lexer("nop 1 add 2 sub 3 and 4 or xor 5 not push dup 8 drop 10")
+    lexer = Lexer("add 2 sub 3 push dup 8 drop 10 print")
     lexeme_list = [lexeme for lexeme in lexer.tokens()]
-    assert lexeme_list[0].kind == TokenKind.NOP
+    assert lexeme_list[0].kind == TokenKind.ADD
     assert lexeme_list[1].kind == TokenKind.NUMBER
-    assert lexeme_list[2].kind == TokenKind.ADD
+    assert lexeme_list[2].kind == TokenKind.SUB
     assert lexeme_list[3].kind == TokenKind.NUMBER
-    assert lexeme_list[4].kind == TokenKind.SUB
-    assert lexeme_list[5].kind == TokenKind.NUMBER
-    assert lexeme_list[6].kind == TokenKind.AND
-    assert lexeme_list[7].kind == TokenKind.NUMBER
-    assert lexeme_list[8].kind == TokenKind.OR
-    assert lexeme_list[9].kind == TokenKind.XOR
-    assert lexeme_list[10].kind == TokenKind.NUMBER
-    assert lexeme_list[11].kind == TokenKind.NOT
-    assert lexeme_list[12].kind == TokenKind.PUSH
-    assert lexeme_list[13].kind == TokenKind.DUP
-    assert lexeme_list[14].kind == TokenKind.NUMBER
-    assert lexeme_list[15].kind == TokenKind.DROP
-    assert lexeme_list[16].kind == TokenKind.NUMBER
-    assert lexeme_list[17].kind == TokenKind.EOF
+    assert lexeme_list[4].kind == TokenKind.PUSH
+    assert lexeme_list[5].kind == TokenKind.DUP
+    assert lexeme_list[6].kind == TokenKind.NUMBER
+    assert lexeme_list[7].kind == TokenKind.DROP
+    assert lexeme_list[8].kind == TokenKind.NUMBER
+    assert lexeme_list[9].kind == TokenKind.PRINT
+    assert lexeme_list[10].kind == TokenKind.EOF
 
